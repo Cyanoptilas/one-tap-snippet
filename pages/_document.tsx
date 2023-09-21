ce12a96656f6448a31d63a16135aa31a83e4df41
Cyanoptila <februarys22@gmail.com>
2023-09-21 12:22:18 +0900

    Revert "MOD AdSenseコード追加"
    This reverts commit 8121164f8bfb22b4505a5dc43f7d14ef36371c2f.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -9,12 +8,6 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap"
-          />
-          <Script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4674670817271557"
-            crossOrigin="anonymous"
-            strategy="afterInteractive"
           />
         </Head>
         <body>
