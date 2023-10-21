45d81e5c52e99eaeba6846c35cefada9e9c64cbc
Cyanoptilas <februarys22@gmail.com>
2023-10-21 17:39:11 +0900

    MOD GoogleAnalyticコード追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,43 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return (
     <SessionProvider session={pageProps.session}>
       <ChakraProvider>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+          }}
+        />
         <Component {...pageProps} />
       </ChakraProvider>
     </SessionProvider>
