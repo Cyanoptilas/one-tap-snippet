cc16b6c282d0be16e73e0cdc290023f0028a5dc3
Cyanoptilas <februarys22@gmail.com>
2023-10-24 19:06:54 +0900

    MOD

--- features/ui/ad/AdCard.tsx
+++ features/ui/ad/AdCard.tsx
@@ -1,37 +1,46 @@
-import React from "react";
-import { useRouter } from "next/router";
-import { Box } from "@chakra-ui/react";
+import { useEffect } from "react";
 
-export type Props = React.ComponentProps<"div">;
-
+// 広告タイプの型
 type AdmaxAdType = {
-  adMaxId: string; // 広告ID
+  admax_id: string; // 広告ID
   type: string; // PC/SP切替広告なら"switch"
 };
 
-declare global {
-  var admaxads: AdmaxAdType[];
-}
+const key: keyof string;
 
-export const AdsCard = ({ adMaxId }: { adMaxId: string }) => {
-  const { asPath } = useRouter();
-
-  React.useEffect(() => {
-    // 広告配信用のタグを挿入する
+// PC/SP切替広告のReactコンポーネント
+export const AdsCard: React.FC<{ id: string }> = (props) => {
+  useEffect(() => {
+    // windowオブジェクトの広告リストを初期化
+    if (!window["admaxads"]) window["admaxads"] = [];
+    // 広告リストを取得
+    const admaxads: AdmaxAdType[] = window["admaxads"];
+    // 広告リストになかったら追加
+    if (!admaxads.some((ad) => ad.admax_id === props.id))
+      admaxads.push({
+        admax_id: props.id,
+        type: "switch",
+      });
+    // 外部JSを読み込んで広告リストを実際に表示
     const tag = document.createElement("script");
     tag.src = "https://adm.shinobi.jp/st/t.js";
     tag.async = true;
     document.body.appendChild(tag);
-
-    try {
-      (globalThis.admaxads = window.admaxads || []).push({
-        adMaxId: adMaxId,
-        type: "switch",
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  }, [asPath]);
-
-  return <div key={asPath} className="admax-switch" data-admax-id={adMaxId}></div>;
+    // アンマウント時にJSタグと広告情報を削除
+    return () => {
+      document.body.removeChild(tag);
+      admaxads.splice(
+        admaxads.findIndex((ad) => ad.admax_id === props.id),
+        1
+      );
+      window["__admax_tag__"] = undefined;
+    };
+  }, []);
+  return (
+    <div
+      className="admax-switch"
+      data-admax-id={props.id}
+      style={{ display: "inline-block" }}
+    />
+  );
 };
