dc43ff26af9da123e5a74520ae0dfe9af80af398
Cyanoptilas <februarys22@gmail.com>
2023-10-24 19:22:42 +0900

    MOD

--- features/ui/SnippetsViewPage.tsx
+++ features/ui/SnippetsViewPage.tsx
@@ -210,206 +210,209 @@ function SnippetsViewPage(props: SnippetsViewPageProps) {
   };
 
   return (
-    <Layout>
-      <StatesContext.Provider value={ContextProps}>
-        <Container
-          maxW="6xl"
-          pt={2}
-          position="relative"
-          height={{ base: "91vh", md: "92vh", lg: "93vh" }}
-          minHeight={{ base: "880px", md: "810px", lg: "860px" }}
-        >
-          <VStack align="center" minHeight="100%">
-            {/* 上部操作系表示エリア */}
-            <Flex
-              justifyContent="space-between"
-              width="100%"
-              flexDirection={{ base: "column", md: "row" }}
-              alignItems="center"
-              minHeight={{ base: "80px", md: "70px", lg: "60px" }}
-            >
-              <Heading
-                as="h1"
-                size={{ base: "md", md: "lg" }}
-                color="blue.600"
-                fontFamily="Poppins, sans-serif"
-                fontStyle="italic"
-                mb={{ base: 4, md: 0 }}
-              >
-                {isTopPage
-                  ? "Snippets"
-                  : isFavoritePage
-                  ? "My Favorites"
-                  : "My Snippets"}
-              </Heading>
-
+    <>
+      <AdsCard id="8d595c264fd4df83aaf278d7cbd76db8" />
+      <Layout>
+        <StatesContext.Provider value={ContextProps}>
+          <Container
+            maxW="6xl"
+            pt={2}
+            position="relative"
+            height={{ base: "91vh", md: "92vh", lg: "93vh" }}
+            minHeight={{ base: "880px", md: "810px", lg: "860px" }}
+          >
+            <VStack align="center" minHeight="100%">
+              {/* 上部操作系表示エリア */}
               <Flex
-                direction={{ base: "column", md: "row" }}
+                justifyContent="space-between"
+                width="100%"
+                flexDirection={{ base: "column", md: "row" }}
                 alignItems="center"
+                minHeight={{ base: "80px", md: "70px", lg: "60px" }}
               >
-                <Flex direction="row" mb={{ base: 2, md: 0 }}>
-                  <SearchBox
-                    searchValue={searchValue}
-                    setSearchValue={setSearchValue}
-                  />
-                </Flex>
+                <Heading
+                  as="h1"
+                  size={{ base: "md", md: "lg" }}
+                  color="blue.600"
+                  fontFamily="Poppins, sans-serif"
+                  fontStyle="italic"
+                  mb={{ base: 4, md: 0 }}
+                >
+                  {isTopPage
+                    ? "Snippets"
+                    : isFavoritePage
+                    ? "My Favorites"
+                    : "My Snippets"}
+                </Heading>
 
                 <Flex
-                  direction="row"
+                  direction={{ base: "column", md: "row" }}
                   alignItems="center"
-                  justifyContent={{ base: "flex-end", md: "flex-start" }}
-                  width={{ base: "100%", lg: "auto" }}
                 >
-                  <RadioGroupOrder order={order} setOrder={setOrder} />
-                  <SortMenu
-                    sort={sort}
-                    setSort={setSort}
-                    order={order}
-                    setPage={setPage}
-                  />
+                  <Flex direction="row" mb={{ base: 2, md: 0 }}>
+                    <SearchBox
+                      searchValue={searchValue}
+                      setSearchValue={setSearchValue}
+                    />
+                  </Flex>
 
-                  {session && session.user?.email && (
-                    <Link href="/register">
-                      <IconButton
-                        icon={<AddIcon />}
-                        colorScheme="blue"
-                        ml={4}
-                        aria-label="新規投稿"
-                      />
-                    </Link>
-                  )}
+                  <Flex
+                    direction="row"
+                    alignItems="center"
+                    justifyContent={{ base: "flex-end", md: "flex-start" }}
+                    width={{ base: "100%", lg: "auto" }}
+                  >
+                    <RadioGroupOrder order={order} setOrder={setOrder} />
+                    <SortMenu
+                      sort={sort}
+                      setSort={setSort}
+                      order={order}
+                      setPage={setPage}
+                    />
+
+                    {session && session.user?.email && (
+                      <Link href="/register">
+                        <IconButton
+                          icon={<AddIcon />}
+                          colorScheme="blue"
+                          ml={4}
+                          aria-label="新規投稿"
+                        />
+                      </Link>
+                    )}
+                  </Flex>
                 </Flex>
               </Flex>
-            </Flex>
 
-            {/* カード表示エリア */}
-            <SnippetCards
-              displayedSnippets={displayedSnippets}
-              favoriteSnippetIds={favoriteSnippetIds}
-            />
+              {/* カード表示エリア */}
+              <SnippetCards
+                displayedSnippets={displayedSnippets}
+                favoriteSnippetIds={favoriteSnippetIds}
+              />
 
-            {/* 広告 */}
-            <AdsCard id="51201e20f99cc4fb6ee72cc7bc373783" />
-            <AdsCard id="8d595c264fd4df83aaf278d7cbd76db8" />
+              {/* 広告 */}
+              <AdsCard id="51201e20f99cc4fb6ee72cc7bc373783" />
+              <AdsCard id="8d595c264fd4df83aaf278d7cbd76db8" />
 
-            {/* 下部ボタンエリア */}
-            <Flex
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
-              width="90%"
-              my={0}
-              position="absolute"
-              bottom="1%"
-            >
-              <Button
-                onClick={() => setPage(page - 1)}
-                isDisabled={page === 0}
-                colorScheme="blue"
-                mr={2}
-              >
-                前のページ
-              </Button>
-              {isSmallScreen ? (
-                <Center
-                  w="50px"
-                  h="30px"
-                  bg="blue.500"
-                  color="white"
-                  borderRadius="md"
-                >
-                  {page + 1}
-                </Center>
-              ) : (
-                Array.from({ length: totalPages }).map((_, idx) => (
-                  <Center
-                    key={idx}
-                    w="45px"
-                    h="30px"
-                    bg={page === idx ? "blue.500" : "gray.100"}
-                    color={page === idx ? "white" : "black"}
-                    borderRadius="md"
-                    mx={1}
-                    cursor="pointer"
-                    onClick={() => setPage(idx)}
-                  >
-                    {idx + 1}
-                  </Center>
-                ))
-              )}
-              <Button
-                onClick={() => setPage(page + 1)}
-                isDisabled={page + 1 >= totalPages}
-                colorScheme="blue"
-                ml={2}
-              >
-                次のページ
-              </Button>
-            </Flex>
-            <AdsCard id="5cbd60f9cbc041709048511822290814" />
-
-            {isTopPage ? (
-              <Tooltip
-                label="ログイン後、+ボタンから新規登録画面に行き、スニペットをコピペ&Enterで簡単に登録することができます！"
-                fontSize="md"
-                placement="auto-start"
-                offset={[5, 7]}
-                hasArrow
-              >
-                <IconButton
-                  icon={<QuestionOutlineIcon />}
-                  isRound
-                  aria-label="ヘルプ"
-                  position="fixed"
-                  bottom={{ base: 9, md: 6 }}
-                  right={0}
-                  variant="ghost"
-                />
-              </Tooltip>
-            ) : session && isMultiSelectMode ? (
+              {/* 下部ボタンエリア */}
               <Flex
-                position="fixed"
-                right={{ base: 1, md: 4 }}
-                bottom={4}
-                zIndex={10}
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                width="90%"
+                my={0}
+                position="absolute"
+                bottom="1%"
               >
-                <Tooltip label="Delete selected snippets" hasArrow>
-                  <IconButton
-                    icon={<DeleteIcon />}
-                    colorScheme="red"
-                    onClick={deleteSelectedSnippets}
-                    mr={2}
-                    aria-label="Delete selected snippets"
-                    isRound
-                    isDisabled={selectedIds.length === 0}
-                  />
-                </Tooltip>
-                <Tooltip label="Exit multi-select mode" hasArrow>
-                  <IconButton
-                    icon={<CloseIcon />}
-                    onClick={() => setIsMultiSelectMode(false)}
-                    aria-label="Exit multi-select mode"
-                    isRound
-                  />
-                </Tooltip>
+                <Button
+                  onClick={() => setPage(page - 1)}
+                  isDisabled={page === 0}
+                  colorScheme="blue"
+                  mr={2}
+                >
+                  前のページ
+                </Button>
+                {isSmallScreen ? (
+                  <Center
+                    w="50px"
+                    h="30px"
+                    bg="blue.500"
+                    color="white"
+                    borderRadius="md"
+                  >
+                    {page + 1}
+                  </Center>
+                ) : (
+                  Array.from({ length: totalPages }).map((_, idx) => (
+                    <Center
+                      key={idx}
+                      w="45px"
+                      h="30px"
+                      bg={page === idx ? "blue.500" : "gray.100"}
+                      color={page === idx ? "white" : "black"}
+                      borderRadius="md"
+                      mx={1}
+                      cursor="pointer"
+                      onClick={() => setPage(idx)}
+                    >
+                      {idx + 1}
+                    </Center>
+                  ))
+                )}
+                <Button
+                  onClick={() => setPage(page + 1)}
+                  isDisabled={page + 1 >= totalPages}
+                  colorScheme="blue"
+                  ml={2}
+                >
+                  次のページ
+                </Button>
               </Flex>
-            ) : (
-              <Tooltip label="カード長押しで削除モードに入る" hasArrow>
-                <IconButton
-                  icon={<QuestionOutlineIcon />}
+              <AdsCard id="5cbd60f9cbc041709048511822290814" />
+
+              {isTopPage ? (
+                <Tooltip
+                  label="ログイン後、+ボタンから新規登録画面に行き、スニペットをコピペ&Enterで簡単に登録することができます！"
+                  fontSize="md"
+                  placement="auto-start"
+                  offset={[5, 7]}
+                  hasArrow
+                >
+                  <IconButton
+                    icon={<QuestionOutlineIcon />}
+                    isRound
+                    aria-label="ヘルプ"
+                    position="fixed"
+                    bottom={{ base: 9, md: 6 }}
+                    right={0}
+                    variant="ghost"
+                  />
+                </Tooltip>
+              ) : session && isMultiSelectMode ? (
+                <Flex
                   position="fixed"
-                  bottom={{ base: 14, md: 10 }}
-                  right={1}
-                  variant="ghost"
-                  aria-label="カード長押しで削除モードに入る"
-                  isRound
-                />
-              </Tooltip>
-            )}
-          </VStack>
-        </Container>
-      </StatesContext.Provider>
-    </Layout>
+                  right={{ base: 1, md: 4 }}
+                  bottom={4}
+                  zIndex={10}
+                >
+                  <Tooltip label="Delete selected snippets" hasArrow>
+                    <IconButton
+                      icon={<DeleteIcon />}
+                      colorScheme="red"
+                      onClick={deleteSelectedSnippets}
+                      mr={2}
+                      aria-label="Delete selected snippets"
+                      isRound
+                      isDisabled={selectedIds.length === 0}
+                    />
+                  </Tooltip>
+                  <Tooltip label="Exit multi-select mode" hasArrow>
+                    <IconButton
+                      icon={<CloseIcon />}
+                      onClick={() => setIsMultiSelectMode(false)}
+                      aria-label="Exit multi-select mode"
+                      isRound
+                    />
+                  </Tooltip>
+                </Flex>
+              ) : (
+                <Tooltip label="カード長押しで削除モードに入る" hasArrow>
+                  <IconButton
+                    icon={<QuestionOutlineIcon />}
+                    position="fixed"
+                    bottom={{ base: 14, md: 10 }}
+                    right={1}
+                    variant="ghost"
+                    aria-label="カード長押しで削除モードに入る"
+                    isRound
+                  />
+                </Tooltip>
+              )}
+            </VStack>
+          </Container>
+        </StatesContext.Provider>
+      </Layout>
+    </>
   );
 }
 
