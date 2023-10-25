89d146bb6580fee4212e5b853a8da4836b534fcc
Cyanoptila <februarys22@gmail.com>
2023-10-25 16:30:15 +0900

    DEL Adsense削除

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,6 @@ import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
-    const id = process.env.ADSENSE_CLIENT_ID;
     return (
       <Html>
         <Head>
@@ -14,12 +13,6 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap"
-          />
-          <Script
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${id}`}
-            crossOrigin="anonymous"
-            strategy="afterInteractive"
           />
         </Head>
         <body>
