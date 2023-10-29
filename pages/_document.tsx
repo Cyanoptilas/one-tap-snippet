8b042c5c11b2c827ee89ab429285bc0e178173b5
Cyanoptila <februarys22@gmail.com>
2023-10-29 18:07:27 +0900

    ADD OGP追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,20 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="ja" prefix="og: http://ogp.me/ns#">
         <Head>
+          <title>ONE-TAP-SNIPPET</title>
+          <meta
+            name="description"
+            content="コピペ&Enterでスニペットを簡単登録！"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="og:title" content="ONE-TAP-SNIPPET" />
+          <meta property="og:description" content="コピペ&Enterでスニペットを簡単登録！"/>
+          <meta property="og:url" content="https://one-tap-snippet.com/" />
+          <meta property="og:site_name" content="ONE-TAP-SNIPPET" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content={process.env.R2_OGP_IMAGE} />
           <meta
             name="google-site-verification"
             content="f_ONqNmOYowRla-ooKXHamNFNxdwsnAI-7bNbY5QIKY"
